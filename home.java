11c0eb691acaca9e774ae01668be62c725f17969
Aniket Mishra <aniket_mishra@srmap.edu.in>
2026-06-22 09:21:07 +0545

    edited home.java

--- home.java
+++ home.java
@@ -1,13 +1,27 @@
 import java.util.*;
+import java.util.Scanner;
+class eg {
+    void display() {
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter a number:");
+    int a = sc.nextInt();
 
+    System.out.println("Print: "+ (a+2));
+    sc.close();
+    }
+}
 public class home {
     public static void main(String[] args) {
+        eg obj = new eg();
+        obj.display();
         System.out.println("Program executed");
 
         try {
-            int a = 10 / 0;
+            int b = 10 / 0;
         } catch (ArithmeticException e) {
             System.out.println("Cannot divide by zero");
         }
     }
 }
+//Output: Program executed 
+//Cannot divide by zero
