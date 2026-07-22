4edc6dea859047691a21e1876329eaad82509023
Aniket Mishra <aniket_mishra@srmap.edu.in>
2026-07-22 21:21:49 +0545

    Largest in arry

---
+++ largest.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+int largest(vector<int>&arr){
+    int Largest = INT_MIN;
+    int n = arr.size();
+    for(int i=0;i<n;i++){
+         if(arr[i]>largest){
+            Largest = max(Largest,arr[i]);
+         }
+    }
+    return Largest;
+}
+int main(){
+    int n ;
+    cin>>n;
+    vector<int>arr(n);
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    cout<<Largest(arr);
+    return 0;
+}
